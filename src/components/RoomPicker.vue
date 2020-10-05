766a45fddde03aae9fae7bccfa0421579ee9b948
Randy Lubin <randylubin@gmail.com>
2020-10-05 08:22:34 -0700

    updated roomPicker to work off of entire URLs, rather than IDs

--- src/components/RoomPicker.vue
+++ src/components/RoomPicker.vue
@@ -78,7 +78,7 @@
           <div class="row mb-4">
             <div class="col-sm">
               Sheet (optional) 
-              <svg v-b-tooltip.hover title="Paste in the ID of a publicly shared Google Sheet that contains the game data. The ID is the section in between '/d/' and '/?edit' (no / included)."  width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-question-circle-fill m-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg v-b-tooltip.hover title="Paste in the entire URL link to your publicly shared Google Sheet that contains the game data."  width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-question-circle-fill m-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"/>
               </svg>
               
@@ -161,10 +161,13 @@ export default {
 
       newPath += "/"
 
-      if(!gSheetID || gSheetID === ""){
+      if(!gSheetID || gSheetID == 'demo' || gSheetID === ""){
         newPath += 'demo'
+      } else if (gSheetID.includes("spreadsheet")){
+        //https://docs.google.com/spreadsheets/d/1xnQJNbKpDafpUziNtCmQy_RR6PY4A0nlC77Tx2QDutg/edit?usp=sharing
+        newPath += gSheetID.substring(gSheetID.indexOf("/d/")+3, gSheetID.indexOf("/edit?"))
       } else {
-        newPath += gSheetID
+        newPath += gSheetID;
       }
 
       newPath += "/" + roomID
