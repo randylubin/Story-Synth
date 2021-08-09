c526d97a6b45fa069c5d25a22f20c5248b4ce55a
Rebecca Dahl <rebeccadahl@gmail.com>
2021-08-10 10:38:33 +1200

    added component for vue tooling, Vetur

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,14 +2,13 @@
     <div class="header-section">
 
         <b-navbar toggleable="md" type="dark" variant="dark" fixed="top" v-if="!$route.params.roomID">
-          <b-navbar-brand href="/">Story Synth</b-navbar-brand>
+          <b-navbar-brand href="/">Amble Story Synth</b-navbar-brand>
           <b-navbar-toggle target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           </b-navbar-toggle>
         
           <b-collapse id="navbarSupportedContent" is-nav>
             <b-navbar-nav>
-                <b-nav-item href="/">Reset game</b-nav-item>
-                <b-nav-item href="https://amble.studio">Back to Amble</b-nav-item>
+                <b-nav-item href="https://amble.studio">Back to Amble.Studio</b-nav-item>
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
