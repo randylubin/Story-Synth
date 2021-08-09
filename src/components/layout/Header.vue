8ae46018b14d9147c9ab316678a8beca7764ac9a
Hailey Cooperrider <haileycooperrider@gmail.com>
2021-08-09 16:21:41 +1000

    hide header for now

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="header-section">
-
-        <b-navbar toggleable="md" type="dark" variant="dark" fixed="top" v-if="!$route.params.roomID">
+  <div class="header-section">
+    <!--         <b-navbar toggleable="md" type="dark" variant="dark" fixed="top" v-if="!$route.params.roomID">
           <b-navbar-brand href="/">Story Synth</b-navbar-brand>
           <b-navbar-toggle target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           </b-navbar-toggle>
@@ -12,15 +11,12 @@
                 <b-nav-item href="https://amble.studio">Back to Amble</b-nav-item>
             </b-navbar-nav>
           </b-collapse>
-        </b-navbar>
-
-    </div>
+        </b-navbar> -->
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app-header',
-}
+  name: "app-header",
+};
 </script>
