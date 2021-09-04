deae327647920539e1c81add25dc9572abbbbbd5
Hailey Cooperrider <haileycooperrider@gmail.com>
2021-09-04 16:51:59 +1000

    remove most prerenders and add arribatown

--- vue.config.js
+++ vue.config.js
@@ -4,26 +4,27 @@ module.exports = {
       registry: undefined,
       renderRoutes: [
         '/',
-        '/Gallery/',
-        '/Games/Aethelreds-Academy/',
-        '/Games/Around-The-Realm/',
-        '/Games/Clash-At-Ikara/',
-        '/Games/Post-Work-Paradise/',
-        '/Games/Raised-By-Mechs/',
-        '/Shuffled/1N5eeyKTVWo5QeGcUV_zYtwtR0DikJCcvcj6w69UkC1w/',
-        '/Shuffled/1N2N8URSswhmxLalPOkzi5d4kdeFiGrsobEmM_ojk0ow/',
-        '/Timed/1yq2AKwaYL1uZrCnEfwgSpC0SPkQAZqnCdjNxH_pm018/',
-        '/Monster/1NgNHy7Qe1R8KhGR2cOmJwL2aOl2tocBemW2HIAKjrvI/',
-        '/SlotMachine/1t5LRUQG9DzMJ3kd8E9DZV7_EbE8J5-Gqhz7TWQ4Y-uU/',
-        '/Phases/1HataDfV2lrA4hfzmLgDjXH09dEMLQV6OT10tVH9G52A/',
-        '/SecretCards/1JwMF02DSxNKtjHp6u-wyznSs-iEG_3DpOobgc17I16o/',
-        '/Shuffled/1rJ3L8E_sBEqiRO8ADQeSAS2UmcK9Xzk0g_F0d_XkkRc/', // Reunion
-        '/Shuffled/1q5LuB6H5F9hxJEZovAS4LJZc6YS8mYs0tOFIcCUp4lA/', //Raised by Mechs,
-        '/Shuffled/16jKRcMytaNLTdqHGWE00dFVaaihzD38ga1hTm1wr-iA/', // working for the corp
-        '/Shuffled/1ghm-XOUZlYaOeHcl0zer4KSpCMaoN5u_X7iWmN2TrjY/', // For the Awesome
-        '/Shuffled/1WLy_IM9DehQbrBNjTdL67Bc-ZiW83INcfxlnzpDMqnA/', // This Heart Within Me Burns
-        '/Shuffled/1XVd4Kgq3X3utOm25JXOsDbhI_jLjAHX8mGzpiT6Sgho/', // Compersion
-        '/Generator/1-yrqoN7ZKeLHg0ynob9ZCpUbr64T24IrpX1RuSFUAwY/', // Umberdred
+        '/Games/ArribaTown/'
+        // '/Gallery/',
+        // '/Games/Aethelreds-Academy/',
+        // '/Games/Around-The-Realm/',
+        // '/Games/Clash-At-Ikara/',
+        // '/Games/Post-Work-Paradise/',
+        // '/Games/Raised-By-Mechs/',
+        // '/Shuffled/1N5eeyKTVWo5QeGcUV_zYtwtR0DikJCcvcj6w69UkC1w/',
+        // '/Shuffled/1N2N8URSswhmxLalPOkzi5d4kdeFiGrsobEmM_ojk0ow/',
+        // '/Timed/1yq2AKwaYL1uZrCnEfwgSpC0SPkQAZqnCdjNxH_pm018/',
+        // '/Monster/1NgNHy7Qe1R8KhGR2cOmJwL2aOl2tocBemW2HIAKjrvI/',
+        // '/SlotMachine/1t5LRUQG9DzMJ3kd8E9DZV7_EbE8J5-Gqhz7TWQ4Y-uU/',
+        // '/Phases/1HataDfV2lrA4hfzmLgDjXH09dEMLQV6OT10tVH9G52A/',
+        // '/SecretCards/1JwMF02DSxNKtjHp6u-wyznSs-iEG_3DpOobgc17I16o/',
+        // '/Shuffled/1rJ3L8E_sBEqiRO8ADQeSAS2UmcK9Xzk0g_F0d_XkkRc/', // Reunion
+        // '/Shuffled/1q5LuB6H5F9hxJEZovAS4LJZc6YS8mYs0tOFIcCUp4lA/', //Raised by Mechs,
+        // '/Shuffled/16jKRcMytaNLTdqHGWE00dFVaaihzD38ga1hTm1wr-iA/', // working for the corp
+        // '/Shuffled/1ghm-XOUZlYaOeHcl0zer4KSpCMaoN5u_X7iWmN2TrjY/', // For the Awesome
+        // '/Shuffled/1WLy_IM9DehQbrBNjTdL67Bc-ZiW83INcfxlnzpDMqnA/', // This Heart Within Me Burns
+        // '/Shuffled/1XVd4Kgq3X3utOm25JXOsDbhI_jLjAHX8mGzpiT6Sgho/', // Compersion
+        // '/Generator/1-yrqoN7ZKeLHg0ynob9ZCpUbr64T24IrpX1RuSFUAwY/', // Umberdred
       ],
       useRenderEvent: true,
       headless: true,
