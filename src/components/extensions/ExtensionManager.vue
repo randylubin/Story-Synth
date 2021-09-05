56cd183a7e934d5cf545aac99197a12a283a196b
Randy Lubin <randylubin@gmail.com>
2021-09-04 17:03:58 -0700

    v1 of journal entries

--- src/components/extensions/ExtensionManager.vue
+++ src/components/extensions/ExtensionManager.vue
@@ -2,6 +2,7 @@
   <div class="">
       <div class="row">
         <div class="col-sm">
+          <app-hexflowerAsExtension :hexflowerAsExtension="Boolean(extensionData.hexflowerAsExtension)" :gSheetForExtension="extensionData.hexflowerAsExtension" :gSheetID="extensionData.hexflowerAsExtension.substring(extensionData.hexflowerAsExtension.indexOf('/d/') + 3, extensionData.hexflowerAsExtension.indexOf('/edit'))" :roomID="$route.params.roomID + '-hexflower-extensions'" v-if="Boolean(extensionData.hexflowerAsExtension)"></app-hexflowerAsExtension>
           <app-staticBox :staticBoxContent="extensionData.staticBoxContent" v-if="extensionData.staticBoxContent"></app-staticBox>
           <app-playerTurnOrder :playerTurnOrder="JSON.parse(extensionData.playerTurnOrder)" :playerTurnOrderHeader="extensionData.playerTurnOrderHeader" :playerTurnOrderButtonLabel="extensionData.playerTurnOrderButtonLabel" :playerTurnOrderFirstVisible="parseInt(extensionData.playerTurnOrderFirstVisible)" :currentCardIndex="roomInfo.currentCardIndex" @process-extension-update="processExtensionUpdate($event)" v-if="extensionList['playerTurnOrder']"></app-playerTurnOrder>
           <app-plusMinus :plusMinus="JSON.parse(extensionData.plusMinus)" :plusMinusTitle="extensionData.plusMinusTitle" @process-extension-update="processExtensionUpdate($event)" v-if="extensionList['plusMinus']"></app-plusMinus>
@@ -9,7 +10,7 @@
           <app-multiEditableLists :multiEditableLists="JSON.parse(extensionData.multiEditableLists)" @process-extension-update="processExtensionUpdate($event)" v-if="extensionList['multiEditableLists']"></app-multiEditableLists>
           <app-diceRoller :diceResults="extensionData.diceRoller.split(',')" :diceRollerTitle="extensionData.diceRollerTitle" @process-extension-update="processExtensionUpdate($event)" v-if="extensionList['diceRoller']"></app-diceRoller>
           <app-standardDeck :standardDeck="JSON.parse(extensionData.standardDeck)" :standardDeckTitle="extensionData.standardDeckTitle" @process-extension-update="processExtensionUpdate($event)" v-if="extensionList['standardDeck']"></app-standardDeck>
-          <app-hexflowerAsExtension :hexflowerAsExtension="Boolean(extensionData.hexflowerAsExtension)" :gSheetForExtension="extensionData.hexflowerAsExtension" :gSheetID="extensionData.hexflowerAsExtension.substring(extensionData.hexflowerAsExtension.indexOf('/d/') + 3, extensionData.hexflowerAsExtension.indexOf('/edit'))" :roomID="$route.params.roomID + '-hexflower-extensions'" v-if="Boolean(extensionData.hexflowerAsExtension)"></app-hexflowerAsExtension>
+          <app-journal :journalEntries="JSON.parse(extensionData.journalEntries)" :journalUpperText="extensionData.journalUpperText" @process-extension-update="processExtensionUpdate($event)" v-if="(extensionList['journalEntries'] || extensionList.journalUpperText)"></app-journal>
         </div>
       </div>
   </div>
@@ -23,6 +24,7 @@ import MultiEditableLists from './MultiEditableLists.vue'
 import DiceRoller from './DiceRoller.vue'
 import StaticBox from './StaticBox.vue'
 import StandardDeck from './StandardDeck.vue'
+import Journal from './Journal.vue'
 import Hexflower from '../formats/Hexflower.vue'
 
 export default {
@@ -35,6 +37,7 @@ export default {
     'app-staticBox': StaticBox,
     'app-playerTurnOrder': PlayerTurnOrder,
     'app-standardDeck': StandardDeck,
+    'app-journal': Journal,
     'app-hexflowerAsExtension': Hexflower,
   },
   props: {
