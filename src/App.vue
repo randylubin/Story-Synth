efe32c33e3921d95d67b0c8a7adc949049456a51
Randy Lubin <randylubin@gmail.com>
2021-09-17 16:15:42 -0700

    improved shadows

--- src/App.vue
+++ src/App.vue
@@ -247,6 +247,16 @@
     margin: 0 10px;
   }
 
+  #app .shadow {
+    box-shadow:
+      0 1px 1px hsl(0deg 0% 0% / 0.075),
+      0 2px 2px hsl(0deg 0% 0% / 0.075),
+      0 4px 4px hsl(0deg 0% 0% / 0.075),
+      0 8px 8px hsl(0deg 0% 0% / 0.075),
+      0 16px 16px hsl(0deg 0% 0% / 0.075) !important
+    ;
+  }
+
   .non-footer-content {
     padding-bottom: 3rem;
   }
