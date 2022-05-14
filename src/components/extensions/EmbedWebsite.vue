8b3a77b600d6f0b27810f05e89916983f3bc0d1d
Randy Lubin <randylubin@gmail.com>
2022-05-13 20:00:16 -0700

    added ability for user to embed a file

--- src/components/extensions/EmbedWebsite.vue
+++ src/components/extensions/EmbedWebsite.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="mb-4 embed-website" :class="{'iframe-expanded': expand}">
-      <div class="card">
+      <div class="card embed-website-message" v-if="!selectedWebsiteURL">
+        <div class="card-body">
+          <div v-dompurify-html="markdownParsedStaticContent"></div>
+          <b-form-input
+            v-model="newURL"
+            class="form-control"
+            placeholder="Paste a link here to embed it in the page"
+            title="Paste an embeddable link here"
+            size="lg" 
+          />
+          <b-button :disabled="!newURL" v-on:click="submitURL()">Embed URL</b-button>
+        </div>
+
+      </div>
+      <div class="card embed-website-by-url" v-if="selectedWebsiteURL">
         <div class="row">
           <div class="col-sm iframe-menu text-right">
             <b-button size="sm" pill variant="outline-secondary" class="m-1" v-if="!expand" v-on:click="toggleExpansion()">
@@ -8,12 +22,15 @@
             </b-button>
             <b-button v-else size="sm" pill variant="outline-secondary" class="m-1" v-on:click="toggleExpansion()">
               <b-icon-arrows-angle-contract></b-icon-arrows-angle-contract>
+            </b-button>
+            <b-button v-if="!hardcodedWebsiteURL" size="sm" pill variant="outline-secondary" class="m-1" v-on:click="clearEmbed()">
+              X
             </b-button>  
           </div>
         </div>
         <div class="row">
           <div class="col-sm">
-            <iframe width="100%" height="1500" :src="embedWebsiteURL" allow="fullscreen" title="Embedded Doc"></iframe>
+            <iframe width="100%" height="1500" :src="selectedWebsiteURL" allow="fullscreen" title="Embedded Doc"></iframe>
           </div>
         </div>
       </div>
@@ -24,18 +41,37 @@
 export default {
   name: 'app-embed-website',
   props: {
-    embedWebsiteURL: String,
+    embedWebsiteMessage: String,
+    selectedWebsiteURL: String,
+    hardcodedWebsiteURL: String,
   },
   data: function() {
     return {
       expand: false,
+      newURL: null,
       error: null
     };
   },
+  computed: {
+    markdownParsedStaticContent: function(){
+      return this.$marked(this.embedWebsiteMessage)
+    }
+  },
   mounted(){
-
+    if (this.hardcodedWebsiteURL) {
+      this.$emit('process-extension-update', ['selectedWebsiteURL',this.hardcodedWebsiteURL])
+    } else if (this.selectedWebsiteURL) {
+      this.$emit('process-extension-update', ['selectedWebsiteURL',this.selectedWebsiteURL])
+    }
   },
   methods: {
+    clearEmbed(){
+      this.newURL = null;
+      this.$emit('process-extension-update', ['selectedWebsiteURL', null]);
+    },
+    submitURL(){
+      this.$emit('process-extension-update', ['selectedWebsiteURL',this.newURL])
+    },
     toggleExpansion(){
       this.expand = !this.expand
     }
