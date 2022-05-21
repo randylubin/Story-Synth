edabc74db522abc65972b1705a7da015dcfcc6b6
Randy Lubin <randylubin@gmail.com>
2022-05-20 23:34:15 -0700

    v1 with journal, and both list types

--- src/components/extensions/DownloadExtensionData.vue
+++ src/components/extensions/DownloadExtensionData.vue
@@ -31,20 +31,45 @@ export default {
       // Filename
       if (this.gameTitle) {
         filename = this.gameTitle.replace(/[^a-z0-9]/gi, '_') + "-data.txt";
-        downloadText = this.gameTitle + "\n\n"
+        downloadText = this.gameTitle + "\n\n\n"
       } else {
         filename = "story-synth-data.txt"
       }
 
       // Editable List
+      let listItems = this.extensionData.editableList ? this.extensionData.editableList.split(', ') : null;
+      if (listItems) {
+        if (this.extensionData.editableListTitle) {
+          downloadText += "# " + this.extensionData.editableListTitle + "\n\n"
+        } else {downloadText += '# List\n\n'}
+
+        for (let i =0; i < listItems.length; i++){
+          downloadText += "- " + listItems[i] + "\n"
+        }
+        downloadText += "\n\n"
+      }
 
       // Multi Editable List
+      let multiLists = this.extensionData.multiEditableLists ? JSON.parse(this.extensionData.multiEditableLists) : {};
+      if (multiLists) {
+        for (const list in multiLists) {
+          if (multiLists[list]['value'].length) {
+            downloadText += "# " + multiLists[list].name + '\n\n'
+            
+            for (let i = 0; i < multiLists[list]['value'].length; i++) {
+              downloadText += "- " + multiLists[list].value[i] + "\n";
+            }
+          }
+        downloadText += "\n\n"
+        }
+      }
+
 
       // Journal Entries
-      let entries = JSON.parse(this.extensionData.journalEntries);
-      if (this.extensionData.journalEntries){
+      let entries = this.extensionData.journalEntries ? JSON.parse(this.extensionData.journalEntries) : null;
+      if (entries){
         if (this.extensionData.journalUpperText){
-          downloadText += "# " + this.extensionData.journalUpperText + "\n\n"
+          downloadText += "# " + this.extensionData.journalUpperText + "\n"
         } else {downloadText += "# Journal Entries\n"}
 
         for (let i = 0; i < entries.length; i++){
