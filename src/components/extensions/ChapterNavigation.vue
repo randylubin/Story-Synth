63376fe43c58d89c5f5ae66f3770539da89d1625
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-06-17 12:56:13 +1000

    first pass on adding chapter nav extension

---
+++ src/components/extensions/ChapterNavigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mb-4 template" v-if="extensionLocation == 'lower'">
+      <div class="row">
+        <div class="col-sm">
+          <div class="card d-flex shadow">
+            <div class="card-body">
+              <h2>Chapter Navigation</h2>
+              <p>You can jump around!</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-template',
+  props: {
+    templateData: Array,
+    extensionLocation: String,
+    extensionData: Object,
+  },
+  data: function() {
+    return {
+      error: null
+    };
+  },
+  mounted(){
+    console.log(this.extensionData);
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+
+</style>
