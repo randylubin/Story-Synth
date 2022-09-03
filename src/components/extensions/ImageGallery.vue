2091542540fba8b4fe81e8c54248563f35047919
Randy Lubin <randylubin@gmail.com>
2022-09-03 10:19:58 -0700

    bugfix

--- src/components/extensions/ImageGallery.vue
+++ src/components/extensions/ImageGallery.vue
@@ -5,16 +5,16 @@
           <div class="card d-flex shadow">
             <div class="card-body">
               <h2 v-if="imageGalleryTitle" class="image-gallery-title">{{imageGalleryTitle}}</h2>
-              <div v-if="!selectedImage">
+              <div v-if="!Number.isInteger(selectedImage)">
                 <figure v-for="(image, index) in imageGallery" v-bind:key="index" class="figure">
                   <img role="button" v-bind:alt="imageCaptions[index]" v-bind:src="image" v-on:click="selectImage(index)" class="m-1 figure-img img-fluid rounded gallery-image">
-                  <figcaption v-if="imageCaptions && imageCaptions[index]" class="figure-caption">{{imageCaptions[index]}}</figcaption>
+                  <figcaption v-if="imageCaptions && imageCaptions[index]" class="figure-caption image-caption">{{imageCaptions[index]}}</figcaption>
                 </figure>
               </div>
-              <div v-if="selectedImage">
+              <div v-if="Number.isInteger(selectedImage)">
                 <figure class="figure">
                   <img v-bind:src="imageGallery[selectedImage]" class="figure-img img-fluid rounded selected-image" v-bind:alt="imageCaptions[selectedImage]">
-                  <figcaption v-if="imageCaptions && imageCaptions[selectedImage]" class="figure-caption">A caption for the above image.</figcaption>
+                  <figcaption v-if="imageCaptions && imageCaptions[selectedImage]" class="figure-caption image-caption">A caption for the above image.</figcaption>
                 </figure>
                 <br><button class="mt-2 btn btn-sm btn-outline-dark deselect-button" v-on:click="deselectImage()">Deselect</button>
               </div> 
@@ -33,7 +33,7 @@ export default {
     imageGallery: Array,
     imageCaptions: Array,
     imageGalleryTitle: String,
-    selectedImage: String,
+    selectedImage: Number,
   },
   data: function() {
     return {
